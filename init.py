c1a99a869176095bc9f9b4903ddac67b9f3321db
vincentzhang <talentzxf@hotmail.com>
2019-11-07 11:27:45 +0000

    Robot init

---
+++ init.py
@@ -0,0 +1,21 @@
+import time
+import serial
+
+
+ser = serial.Serial(
+
+    port='/dev/ttyAMA0',
+    baudrate = 115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+
+for i in range(17):
+    cmd='"#%03dP1500T0150!"'%i
+    print cmd
+    ser.write(cmd)
+	
+	
+ser.close()
