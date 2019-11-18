b14bcca507e762266b187b7d5555275eb3bad09d
Zhang <talentzxf@hotmail.com>
2019-11-18 15:38:29 +0800

    Added linear interpolator

--- src/RobotControl/naivecontrol.py
+++ src/RobotControl/naivecontrol.py
@@ -1,10 +1,18 @@
 import time
 
 
-class ForwardController:
-    def __init__(self, start_time):
-        self.start_time = start_time
+class LinerInterpolator:
+    def __init__(self, start, end, steps):
+        self.start = start
+        self.end = end
+        self.t = 0.0  # from 0.0 to 1.0
+        self.delta = 1.0 / steps
 
+    def get_result(self):
+        cur_position_x = self.start[0] + self.t * (self.end[0] - self.start[0])
+        cur_position_y = self.start[1] + self.t * (self.end[1] - self.start[1])
+        self.t += self.delta
+        yield [cur_position_x, cur_position_y]
 
 
 class NavieControl:
@@ -38,4 +46,4 @@ class NavieControl:
             leg.set_end_pos_local([self.allLegsHeight, 0, 0.3])
 
     def robotGo(self):
-        self.internallController = ForwardController(time.time())
+        self.internallController = ForwardStatus()
