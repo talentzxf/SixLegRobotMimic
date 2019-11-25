3c77ec401f0d06c0191c9f7c4d259b32d894cc5d
Zhang <vincentzhang@ea.com>
2019-11-25 19:07:25 +0800

    Added leg link map

--- src/webserver.py
+++ src/webserver.py
@@ -22,22 +22,6 @@ class IndexResource(Resource):
 
 
 class RobotResource(Resource):
-    leg_link_angle_map = {
-        0: {0: 0, 1: 0, 2: 0},
-        1: {0: 0, 1: 0, 2: 0},
-        2: {0: 0, 1: 0, 2: 0},
-        3: {0: 0, 1: 0, 2: 0},
-        4: {0: 0, 1: 0, 2: 0},
-        5: {0: 0, 1: 0, 2: 0}
-    }
-
-    leg_link_map = [[0, 1, 2],
-                    [3, 4, 5],
-                    [6, 7, 8],
-                    [9, 10, 11],
-                    [12, 13, 14],
-                    [15, 16, 17]]
-
     add_args = {"angle": fields.Float(required=True)}
 
     def convert_angle(self, angle):
